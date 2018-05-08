d3e799ba9f624787d51fbff6d6f5098a38431ae4
CMIAG\LGU <lucagg59@gmail.com>
2018-05-08 09:00:27 +0200

    add https://www.codewars.com/kata/558fc85d8fd1938afb000014

---
+++ Codewars/Codewars/SumTwoLowestIntegers.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    class SumTwoLowestIntegers
+    {
+        public static int sumTwoSmallestNumbers(int[] numbers)
+        {
+            var lowIntegers = new int[2];
+
+            lowIntegers[0] = getSmallestNumber(numbers);
+
+            var list = numbers.ToList();
+            list.Remove(lowIntegers[0]);
+
+            lowIntegers[1] = getSmallestNumber(list.ToArray());
+
+            return lowIntegers[0] + lowIntegers[1];
+        }
+
+        public static int getSmallestNumber(int[] numbers)
+        {
+            int lowest = numbers[0];
+
+            foreach (int i in numbers)
+            {
+                if (i < lowest)
+                    lowest = i;
+            }
+
+            return lowest;
+        }
+    }
+}
