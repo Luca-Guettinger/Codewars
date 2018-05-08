9f9d55493ceed9d9320d14e0cbc48480602abb9b
CMIAG\LGU <lucagg59@gmail.com>
2018-05-08 08:51:02 +0200

    add https://www.codewars.com/kata/array-dot-diff/csharp

---
+++ Codewars/Codewars/ArrayDifference.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    class ArrayDifference
+    {
+        public static int[] ArrayDiff(int[] a, int[] b)
+        {
+
+            List<int> list = a.OfType<int>().ToList();
+            foreach (int int1 in a)
+            {
+                foreach (int int2 in b)
+                {
+                    if (int1 == int2)
+                    {
+                        list.Remove(int1);
+                    }
+                }
+            }
+
+            return list.ToArray<int>();
+        }
+    }
+}
