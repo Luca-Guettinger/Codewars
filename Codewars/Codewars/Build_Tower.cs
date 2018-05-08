093294bc2d174cd88a5d229deef50e8083011748
CMIAG\LGU <lucagg59@gmail.com>
2018-05-08 08:54:26 +0200

    add https://www.codewars.com/kata/576757b1df89ecf5bd00073b

---
+++ Codewars/Codewars/Build_Tower.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    class Build_Tower
+    {
+        public static string[] TowerBuilder(int nFloors)
+        {
+            string[] output = new string[nFloors];
+
+            for (int i = 0; i < nFloors; i++)
+            {
+                output[i] = BuildFloor(nFloors, i);
+            }
+            return output;
+        }
+
+
+        public static string BuildFloor(int floors, int floor)
+        {
+            floors = floors + 1;
+            floor = floor + 1;
+            int bricksLastFloor = 1 + 2 * (floors - 1);
+            int bricksCurrentFloor = 1 + 2 * (floor - 1);
+            int diffrence = bricksLastFloor - bricksCurrentFloor;
+
+            string clearSpace = "";
+            for (int i = 0; i < ((diffrence / 2) - 1); i++)
+            {
+                clearSpace += " ";
+            }
+            string bricks = "";
+            for (int i = 0; i < bricksCurrentFloor; i++)
+            {
+                bricks += "*";
+            }
+
+            return clearSpace + bricks + clearSpace;
+        }
+
+    }
+}
